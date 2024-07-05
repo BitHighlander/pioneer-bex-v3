640f3f82802ac632ffe60cb55b8760eaa61666ad
bithighlander <bithighlander@gmail.com>
2024-07-05 15:07:46 -0600

    signing no confirm working

--- chrome-extension/lib/background/index.ts
+++ chrome-extension/lib/background/index.ts
@@ -2,7 +2,8 @@ import 'webextension-polyfill';
 import { exampleThemeStorage } from '@chrome-extension-boilerplate/storage';
 import packageJson from '../../package.json'; // Adjust the path as needed
 import { onStartKeepkey } from './keepkey';
-
+import { handleEthereumRequest } from './methods';
+import { JsonRpcProvider } from 'ethers';
 const TAG = " | background | ";
 console.log('background script loaded');
 console.log('Version:', packageJson.version);
@@ -10,6 +11,18 @@ console.log('Version:', packageJson.version);
 exampleThemeStorage.get().then(theme => {
   console.log('theme', theme);
 });
+
+const EIP155_CHAINS = {
+  'eip155:1': {
+    chainId: 1,
+    name: 'Ethereum',
+    logo: '/chain-logos/eip155-1.png',
+    rgb: '99, 125, 234',
+    rpc: 'https://eth.llamarpc.com',
+    namespace: 'eip155',
+  },
+};
+const provider = new JsonRpcProvider(EIP155_CHAINS['eip155:1'].rpc);
 
 //Begin KK
 let ADDRESS = '';
@@ -35,46 +48,46 @@ const onStart = async function () {
 };
 onStart();
 
-async function handleEthereumRequest(method, params) {
-  const tag = 'ETH_MOCK | handleEthereumRequest | ';
-
-  // const mockedAddress = '0x141D9959cAe3853b035000490C03991eB70Fc4aC';
-  const mockResponses = {
-    eth_accounts: [ADDRESS],
-    eth_requestAccounts: [ADDRESS],
-    eth_chainId: '0x1',
-    net_version: '1',
-    eth_getBlockByNumber: {
-      baseFeePerGas: '0x1',
-    },
-    eth_sign: '0xMockedSignature',
-    personal_sign: '0xMockedPersonalSignature',
-    eth_signTypedData: '0xMockedTypedDataSignature',
-    eth_signTypedData_v3: '0xMockedTypedDataV3Signature',
-    eth_signTypedData_v4: '0xMockedTypedDataV4Signature',
-    eth_getEncryptionPublicKey: '0xMockedEncryptionPublicKey',
-    eth_decrypt: '0xMockedDecryptionResult',
-    eth_sendTransaction: '0xMockedTransactionHash',
-    wallet_addEthereumChain: true,
-    wallet_switchEthereumChain: true,
-    wallet_watchAsset: true,
-    wallet_requestPermissions: [{ parentCapability: 'eth_accounts' }],
-    wallet_getPermissions: [{ parentCapability: 'eth_accounts' }],
-  };
-
-
-  console.log(tag, 'ADDRESS:', ADDRESS);
-  console.log(tag, 'Handling method:', method);
-  console.log(tag, 'With params:', params);
-
-  if (Object.prototype.hasOwnProperty.call(mockResponses, method)) {
-    console.log(tag, 'Mocking response for method:', method);
-    return mockResponses[method];
-  } else {
-    console.log(tag, 'No mock response for method:', method);
-    throw new Error(`Mock response for method ${method} not implemented`);
-  }
-}
+// async function handleEthereumRequest(method, params) {
+//   const tag = 'ETH_MOCK | handleEthereumRequest | ';
+//
+//   // const mockedAddress = '0x141D9959cAe3853b035000490C03991eB70Fc4aC';
+//   const mockResponses = {
+//     eth_accounts: [ADDRESS],
+//     eth_requestAccounts: [ADDRESS],
+//     eth_chainId: '0x1',
+//     net_version: '1',
+//     eth_getBlockByNumber: {
+//       baseFeePerGas: '0x1',
+//     },
+//     eth_sign: '0xMockedSignature',
+//     personal_sign: '0xMockedPersonalSignature',
+//     eth_signTypedData: '0xMockedTypedDataSignature',
+//     eth_signTypedData_v3: '0xMockedTypedDataV3Signature',
+//     eth_signTypedData_v4: '0xMockedTypedDataV4Signature',
+//     eth_getEncryptionPublicKey: '0xMockedEncryptionPublicKey',
+//     eth_decrypt: '0xMockedDecryptionResult',
+//     eth_sendTransaction: '0xMockedTransactionHash',
+//     wallet_addEthereumChain: true,
+//     wallet_switchEthereumChain: true,
+//     wallet_watchAsset: true,
+//     wallet_requestPermissions: [{ parentCapability: 'eth_accounts' }],
+//     wallet_getPermissions: [{ parentCapability: 'eth_accounts' }],
+//   };
+//
+//
+//   console.log(tag, 'ADDRESS:', ADDRESS);
+//   console.log(tag, 'Handling method:', method);
+//   console.log(tag, 'With params:', params);
+//
+//   if (Object.prototype.hasOwnProperty.call(mockResponses, method)) {
+//     console.log(tag, 'Mocking response for method:', method);
+//     return mockResponses[method];
+//   } else {
+//     console.log(tag, 'No mock response for method:', method);
+//     throw new Error(`Mock response for method ${method} not implemented`);
+//   }
+// }
 
 console.log('background loaded');
 console.log("Edit 'chrome-extension/lib/background/index.ts' and save to reload.");
@@ -91,8 +104,10 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const { method, params } = message;
     console.log(tag, 'method:', method);
     console.log(tag, 'params:', params);
+    console.log(tag, 'ADDRESS:', ADDRESS);
+    // console.log(tag, 'KEEPKEY_SDK:', KEEPKEY_SDK);
 
-    handleEthereumRequest(method, params)
+    handleEthereumRequest(method, params, provider, KEEPKEY_SDK, ADDRESS)
         .then(result => {
           sendResponse(result);
         })
---
+++ chrome-extension/lib/background/methods.ts
@@ -0,0 +1,234 @@
+import { JsonRpcProvider } from 'ethers';
+
+
+export const handleEthereumRequest = async (method: string, params: any[], provider: JsonRpcProvider, KEEPKEY_SDK: any, ADDRESS: string): Promise<any> => {
+    const tag = 'ETH_MOCK | handleEthereumRequest | ';
+    console.log(tag, 'method:', method);
+    console.log(tag, 'params:', params);
+
+      // const mockResponses = {
+      //   eth_accounts: [ADDRESS],
+      //   eth_requestAccounts: [ADDRESS],
+      //   eth_chainId: '0x1',
+      //   net_version: '1',
+      //   eth_getBlockByNumber: {
+      //     baseFeePerGas: '0x1',
+      //   },
+      //   eth_sign: '0xMockedSignature',
+      //   personal_sign: '0xMockedPersonalSignature',
+      //   eth_signTypedData: '0xMockedTypedDataSignature',
+      //   eth_signTypedData_v3: '0xMockedTypedDataV3Signature',
+      //   eth_signTypedData_v4: '0xMockedTypedDataV4Signature',
+      //   eth_getEncryptionPublicKey: '0xMockedEncryptionPublicKey',
+      //   eth_decrypt: '0xMockedDecryptionResult',
+      //   eth_sendTransaction: '0xMockedTransactionHash',
+      //   wallet_addEthereumChain: true,
+      //   wallet_switchEthereumChain: true,
+      //   wallet_watchAsset: true,
+      //   wallet_requestPermissions: [{ parentCapability: 'eth_accounts' }],
+      //   wallet_getPermissions: [{ parentCapability: 'eth_accounts' }],
+      // };
+
+
+    try {
+        switch (method) {
+            case 'eth_chainId':
+                console.log(tag, 'Returning eth_chainId:', '0x1');
+                return '0x1';
+            case 'net_version':
+                console.log(tag, 'Returning net_version:', '1');
+                return '1';
+            case 'eth_getBlockByNumber':
+                console.log(tag, 'Returning eth_getBlockByNumber:', { baseFeePerGas: '0x1' });
+                return { baseFeePerGas: '0x1' };
+            case 'wallet_addEthereumChain':
+            case 'wallet_switchEthereumChain':
+            case 'wallet_watchAsset':
+                console.log(tag, method+ ' Returning true');
+                //TODO change networks
+                return true;
+            case 'wallet_getPermissions':
+            case 'wallet_requestPermissions':
+                return [{ parentCapability: 'eth_accounts' }];
+            case 'eth_accounts':
+                console.log(tag, 'Returning eth_accounts:', [ADDRESS]);
+                return [ADDRESS];
+            case 'eth_requestAccounts':
+                console.log(tag, 'Returning eth_requestAccounts:', [ADDRESS]);
+                return [ADDRESS];
+            case 'eth_sign':
+                console.log(tag, 'Calling signMessage with:', params[1]);
+                return await signMessage(params[1], KEEPKEY_SDK);
+            case 'eth_sendTransaction':
+                console.log(tag, 'Calling sendTransaction with:', params[0]);
+                return sendTransaction(params, provider, KEEPKEY_SDK, ADDRESS);
+            case 'eth_signTransaction':
+                console.log(tag, 'Calling signTransaction with:', params[0]);
+                return await signTransaction(params[0], provider, KEEPKEY_SDK);
+            case 'eth_sendRawTransaction':
+                console.log(tag, 'Calling broadcastTransaction with:', params[0], 'and chainId 1');
+                return await broadcastTransaction(params[0], '1', provider); // Assuming chainId is 1
+            case 'eth_signTypedData':
+                console.log(tag, 'Calling signTypedData with:', params[1]);
+                return await signTypedData(params[1], KEEPKEY_SDK);
+            default:
+                console.log(tag, `Method ${method} not supported`);
+                throw new Error(`Method ${method} not supported`);
+        }
+    } catch (error) {
+        console.error(tag, `Error processing method ${method}:`, error);
+        throw error;
+    }
+};
+
+const signMessage = async (message: any, KEEPKEY_SDK: any) => {
+    try {
+        console.log('signMessage: ', message);
+        console.log('KEEPKEY_SDK.ETH.walletMethods: ', KEEPKEY_SDK.ETH.walletMethods);
+
+        const address = KEEPKEY_SDK.ETH.wallet.address;
+        const messageFormatted = `0x${Buffer.from(
+            Uint8Array.from(typeof message === 'string' ? new TextEncoder().encode(message) : message),
+        ).toString('hex')}`;
+        return KEEPKEY_SDK.eth.ethSign({ address, message: messageFormatted });
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+};
+
+const signTransaction = async (transaction: any, provider: JsonRpcProvider, KEEPKEY_SDK: any) => {
+    const tag = ' | signTransaction | ';
+    try {
+        console.log(tag, '**** transaction: ', transaction);
+
+        if (!transaction.from) throw Error('invalid tx missing from');
+        if (!transaction.to) throw Error('invalid tx missing to');
+        if (!transaction.chainId) throw Error('invalid tx missing chainId');
+
+        const nonce = await provider.getTransactionCount(transaction.from, 'pending');
+        transaction.nonce = `0x${nonce.toString(16)}`;
+
+        const feeData = await provider.getFeeData();
+        console.log('feeData: ', feeData);
+        transaction.gasPrice = `0x${BigInt(feeData.gasPrice || '0').toString(16)}`;
+        transaction.maxFeePerGas = `0x${BigInt(feeData.maxFeePerGas || '0').toString(16)}`;
+        transaction.maxPriorityFeePerGas = `0x${BigInt(feeData.maxPriorityFeePerGas || '0').toString(16)}`;
+
+        try {
+            const estimatedGas = await provider.estimateGas({
+                from: transaction.from,
+                to: transaction.to,
+                data: transaction.data,
+            });
+            console.log('estimatedGas: ', estimatedGas);
+            transaction.gas = `0x${estimatedGas.toString(16)}`;
+        } catch (e) {
+            transaction.gas = `0x${BigInt('1000000').toString(16)}`;
+        }
+
+        const input: any = {
+            from: transaction.from,
+            addressNList: [2147483692, 2147483708, 2147483648, 0, 0],
+            data: transaction.data || '0x',
+            nonce: transaction.nonce,
+            gasLimit: transaction.gas,
+            gas: transaction.gas,
+            value: transaction.value || '0x0',
+            to: transaction.to,
+            chainId: `0x${transaction.chainId.toString(16)}`,
+            gasPrice: transaction.gasPrice,
+            maxFeePerGas: transaction.maxFeePerGas,
+            maxPriorityFeePerGas: transaction.maxPriorityFeePerGas,
+        };
+
+        console.log(`${tag} Final input: `, input);
+        console.log(`${tag} KEEPKEY_SDK: `, KEEPKEY_SDK);
+        const output = await KEEPKEY_SDK.eth.ethSignTransaction(input);
+        console.log(`${tag} Transaction output: `, output);
+
+        return output.serialized;
+    } catch (e) {
+        console.error(`${tag} Error: `, e);
+        throw e;
+    }
+};
+
+const signTypedData = async (params: any, KEEPKEY_SDK: any) => {
+    const tag = ' | signTypedData | ';
+    try {
+        console.log(tag, '**** params: ', params);
+        const signedMessage = await KEEPKEY_SDK.keepkeySdk.eth.ethSignTypedData({
+            address: KEEPKEY_SDK.ETH.wallet.address,
+            addressNList: [2147483692, 2147483708, 2147483648, 0, 0],
+            typedData: params,
+        });
+        console.log(tag, '**** signedMessage: ', signedMessage);
+        return signedMessage;
+    } catch (e) {
+        console.error(`${tag} Error: `, e);
+        throw e;
+    }
+};
+
+const broadcastTransaction = async (signedTx: string, networkId: string, provider: JsonRpcProvider) => {
+    try {
+        const receipt = await provider.send('eth_sendRawTransaction', [signedTx]);
+        console.log('Transaction receipt:', receipt);
+
+        return receipt;
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+};
+
+const sendTransaction = async (transaction: any, provider: JsonRpcProvider, KEEPKEY_SDK: any, ADDRESS: string) => {
+    const tag = ' | sendTransaction | ';
+    try {
+        // chrome.runtime.sendMessage({ action: 'openPopup', request: transaction }, (response: any) => {
+        //     console.log('Popup open response: ', response);
+        //     console.log('response:', response);
+        // });
+        //
+        // const waitForUserDecision = () => {
+        //     return new Promise<{ decision: string }>((resolve, reject) => {
+        //         const listener = (message: any, sender: any, sendResponse: any) => {
+        //             if (message.action === 'responsePopup') {
+        //                 console.log(tag, '****** RESPONSE FROM POPUP ******');
+        //                 console.log(tag, 'Decision:', message.decision);
+        //                 chrome.runtime.onMessage.removeListener(listener);
+        //                 resolve(message);
+        //             }
+        //             return false;
+        //         };
+        //         chrome.runtime.onMessage.addListener(listener);
+        //     });
+        // };
+        //
+        // const userResponse = await waitForUserDecision();
+
+        const userResponse = { decision: 'accept' };
+
+        if (userResponse.decision === 'accept') {
+            console.log(tag, 'User accepted the request');
+            console.log(tag, 'transaction:', transaction);
+            const params = transaction[0];
+            const chainId = '0x1';
+            params.chainId = chainId;
+            params.from = ADDRESS;
+            const signedTx = await signTransaction(params, provider, KEEPKEY_SDK);
+            console.log(tag, 'signedTx:', signedTx);
+
+            //TODO broadcast transaction
+
+            const result = '0x1234567890abcdef';
+            return result;
+        } else if (userResponse.decision === 'reject') {
+            console.log(tag, 'User rejected the request');
+            throw new Error('User rejected the transaction');
+        }
+    } catch (e) {
+        console.error(e);
+    }
+};
